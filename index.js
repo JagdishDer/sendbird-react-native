a5e2a2896a643aade3ab686fc1a25e758ea42f37
JagdishDer <jagdishder80@gmail.com>
2022-04-26 11:43:03 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+/**
+ * @format
+ */
+import { AppRegistry, Platform } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
+
+import App from './App';
+import { name as appName } from './app.json';
+import { onRemoteMessage } from './src/utils';
+
+if (Platform.OS !== 'ios') {
+  messaging().setBackgroundMessageHandler(onRemoteMessage);
+}
+
+AppRegistry.registerComponent(appName, () => App);
+AppRegistry.runApplication('App', {
+  rootTag: document.getElementById('root'),
+});
